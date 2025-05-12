5bc225bb2e14ebc04673f58d58662609731da746
Lertoon Wang <wangkangdi21@gmail.com>
2025-05-12 17:59:31 +0800

    updated the dataset link

--- download_resources.sh
+++ download_resources.sh
@@ -48,12 +48,12 @@ if [[ "$sample_rate" == "8k" || "$sample_rate" == "all" ]]; then
     mkdir -p HRSP2mix_8k/
     if [[ "$version" == "raw" || "$version" == "all" ]]; then
         # wget -O temp_8k_raw.tar.gz https://archive.org/download/hrsp-2mix-8000-raw/HRSP2mix_8000_raw.zip
-        wget -O https://archive.org/download/hrsp-2mix-8k-raw/HRSP2mix_8k_raw.zip
+        wget -O https://archive.org/download/hrsp-2mix-8000-human-raw/HRSP2mix_8000_human_raw.zip
         tar -xzf temp_8k_raw.tar.gz -C /HRSP2mix_8k/
         rm temp_8k_raw.tar.gz
     fi
     if [[ "$version" == "clean" || "$version" == "all" ]]; then
-        wget -O temp_8k_clean.tar.gz https://archive.org/download/hrsp-2mix-8k-clean/HRSP2mix_8k_clean.zip
+        wget -O temp_8k_clean.tar.gz https://archive.org/download/hrsp-2mix-8000-human-clean-0512/HRSP2mix_8000_human_clean.zip
         tar -xzf temp_8k_clean.tar.gz -C HRSP2mix_8k/
         rm temp_8k_clean.tar.gz
     fi
@@ -62,12 +62,12 @@ fi
 if [[ "$sample_rate" == "16k" || "$sample_rate" == "all" ]]; then
     mkdir -p HRSP2mix_16k/
     if [[ "$version" == "raw" || "$version" == "all" ]]; then
-        wget -O temp_16k_raw.tar.gz https://archive.org/download/hrsp-2mix-16k-raw/HRSP2mix_16k_raw.zip
+        wget -O temp_16k_raw.tar.gz https://archive.org/download/hrsp-2mix-16000-human-raw-0512/HRSP2mix_16000_human_raw.zip
         tar -xzf temp_16k_raw.tar.gz -C HRSP2mix_16k/
         rm temp_16k_raw.tar.gz
     fi
     if [[ "$version" == "clean" || "$version" == "all" ]]; then
-        wget -O temp_16k_clean.tar.gz https://archive.org/download/hrsp-2mix-16k-clean/HRSP2mix_16k_clean.zip
+        wget -O temp_16k_clean.tar.gz https://archive.org/download/hrsp-2mix-16000-human-clean-0512/HRSP2mix_16000_human_clean.zip
         tar -xzf temp_16k_clean.tar.gz -C HRSP2mix_16k/
         rm temp_16k_clean.tar.gz
     fi
